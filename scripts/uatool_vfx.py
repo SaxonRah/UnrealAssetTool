10d7adf98233b9a768cf61aa3f92247d5ecd4929
Robert Valentine <paraboliclabs@gmail.com>
2026-09-03 00:10:30 -0400

    AnimNext/UAF: install capability promotion

--- scripts/uatool_vfx.py
+++ scripts/uatool_vfx.py
@@ -12,6 +12,7 @@ import uatool_capabilities as _capabilities
 import uatool_smartobject_capabilities as _smartobject_capabilities
 import uatool_ai_perception_capabilities as _ai_perception_capabilities
 import uatool_dataflow_chaos_capabilities as _dataflow_chaos_capabilities
+import uatool_uaf_capabilities as _uaf_capabilities
 import uatool_inspect as _inspect
 import uatool_project_intelligence as _project_intelligence
 import uatool_smartobject_evidence as _smartobject_evidence
@@ -122,6 +123,7 @@ if not getattr(_build_perf, "_systems_schema10_composition_installed", False):
 _smartobject_capabilities.install(_capabilities)
 _ai_perception_capabilities.install(_capabilities)
 _dataflow_chaos_capabilities.install(_capabilities)
+_uaf_capabilities.install(_capabilities)
 _systems_only_derive_deferred.install()
 _capabilities.install()
 _inspect.install()
