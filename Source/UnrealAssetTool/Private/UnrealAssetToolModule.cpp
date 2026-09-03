782f2ad786cf9f894621bb281d433694c6ce0f38
Robert Valentine <paraboliclabs@gmail.com>
2026-09-03 03:53:03 -0400

    Animation: force mesh physics pass from module startup

--- Source/UnrealAssetTool/Private/UnrealAssetToolModule.cpp
+++ Source/UnrealAssetTool/Private/UnrealAssetToolModule.cpp
@@ -1,4 +1,31 @@
+#include "UnrealAssetToolAnimationMeshPhysicsScanner.h"
+
+#include "Misc/CommandLine.h"
+#include "Misc/CoreDelegates.h"
+#include "Misc/Parse.h"
 #include "Modules/ModuleManager.h"
+
+namespace
+{
+void RunAnimationMeshPhysicsPass()
+{
+    FString RunCommandlet;
+    FParse::Value(FCommandLine::Get(), TEXT("run="), RunCommandlet);
+    if (!RunCommandlet.Equals(TEXT("UnrealAssetToolWorld"), ESearchCase::IgnoreCase))
+    {
+        return;
+    }
+
+    FString OutputDir;
+    FParse::Value(FCommandLine::Get(), TEXT("Output="), OutputDir);
+
+    FString Error;
+    if (!UnrealAssetToolAnimationMeshPhysics::RunScan(OutputDir, Error))
+    {
+        UE_LOG(LogTemp, Error, TEXT("UnrealAssetToolAnimationMeshPhysics: %s"), *Error);
+    }
+}
+}
 
 class FUnrealAssetToolModule final : public IModuleInterface
 {
@@ -6,6 +33,7 @@ public:
     virtual void StartupModule() override
     {
         UE_LOG(LogTemp, Display, TEXT("UnrealAssetTool: editor module loaded."));
+        FCoreDelegates::GetOnPostEngineInit().AddStatic(&RunAnimationMeshPhysicsPass);
     }
 };
 
